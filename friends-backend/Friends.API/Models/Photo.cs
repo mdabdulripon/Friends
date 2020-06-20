fdae9d20fffd3532bb32ba587fe4bbdf6e3b0f04
Abdul Ripon <mdabdulripon@gmail.com>
2020-06-20 17:43:13 -0500

    feat(Update the user model)

---
+++ friends-backend/Friends.API/Models/Photo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Friends.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime AddedDate { get; set; }
+        public bool IsMain { get; set; }
+        // if we add this following information we can have cascade delete instance of restricted delete
+        public User user { get; set; }
+        public int UserId { get; set; }
+    }
+}
--- friends-backend/Friends.API/Models/User.cs
+++ friends-backend/Friends.API/Models/User.cs
@@ -11,5 +11,18 @@ namespace Friends.API.Models
         public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
