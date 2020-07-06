3768b3110acc44cbc26c6382cfab83dccbe21183
Abdul Ripon <mdabdulripon@gmail.com>
2020-07-05 19:00:44 -0500

    feat(create the edit user info api.)

--- friends-backend/Friends.API/Controllers/UsersController.cs
+++ friends-backend/Friends.API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using Friends.API.Dtos;
@@ -42,6 +43,23 @@ namespace Friends.API.Controllers
             return Ok(userToReturn);
         }
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdate)
+        {
+
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+            var user = await _repo.GetUser(id);
+            _mapper.Map(userForUpdate, user);
+
+            if (await _repo.SaveAll())
+            {
+                return NoContent();
+            }
+            throw new Exception($"Updating user {id} failed on save");
+        }
 
     }
 }
